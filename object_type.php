c41801c907cbe567327ba1dd836a5d742ec3a88e
DinaApriansyah <55500629+DinaApriansyah@users.noreply.github.com>
2019-10-10 10:53:06 +0700

    merubah file object_type.php menjadi inheritnce.php

---
+++ object_type.php
@@ -0,0 +1,46 @@
+<?php
+
+
+
+
+class Produk {
+   public $judul  , 
+   		  $penulis  ,
+   		  $penerbit  ,
+   		  $harga ;
+
+
+   	public function __construct($judul = "judul", $penulis="penulis", $penerbit= "penerbit", $harga=0){
+   		$this->judul = $judul;
+   		$this->penulis = $penulis;
+   		$this->penerbit = $penerbit;
+   		$this->harga = $harga;
+   	}
+
+  	public function getLabel(){
+  		return "$this->penulis, $this->penerbit";
+
+  	}
+}
+
+
+class CetakInfoProduk{
+	public function cetak(Produk $produk){
+		$str = " {$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})"; 
+		return $str;
+	}
+}
+
+
+  $produk1 = new Produk("naruto", "Masasahi Kisimoto","Shonen Jump", 30000);
+  $produk2 = new Produk("uncharted", "Neil Druckmann", "Sony Computer", 250000);
+  $produk3 = new Produk("Dragon Ball");
+
+  echo "Komik :" . $produk1->getLabel();
+  echo "<br>";
+  echo "Game :" . $produk2->getLabel();
+  echo "<br>";
+  $infoProduk = new CetakInfoProduk();
+  echo $infoProduk-> cetak($produk1);
+
+ ?>
